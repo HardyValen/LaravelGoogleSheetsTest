6f0644e98bbc8c4c2bf615abbbcbe9911d2cf62b
HardyValen <hardyvalen@gmail.com>
2020-06-07 03:59:21 +0700

    Test

---
+++ app/Http/Controllers/SheetsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SheetsController extends Controller
+{
+    public function viewSheets(){
+        $client = new \Google_Client();
+        $client->setApplicationName('SXCGrandSummit');
+        $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+        $client->setAccessType('offline');
+        $client->setAuthConfig(__DIR__ . '/../../../storage/credentials.json');
+
+        $service = new \Google_Service_Sheets($client);
+        $spreadsheetId = "1XsMr5U1kWZ0lu8jQrUAv7yS0Zz_0MO1ux4B2d2YzJKE";
+
+        $range = "DataSheet!A1:C4";
+        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+        $sheets = ["dataset" => $response->getValues()];
+
+        return view("sheets", $sheets);
+    }
+}
